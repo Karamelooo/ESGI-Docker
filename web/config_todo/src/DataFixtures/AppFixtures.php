ab2cfe03c8cc928d43cfa8f1a57c7bb6fe8a6056
Léo Guirado <leo.guirado@routard.com>
2024-05-14 17:37:42 +0200

    finish

--- web/config_todo/src/DataFixtures/AppFixtures.php
+++ web/config_todo/src/DataFixtures/AppFixtures.php
@@ -10,9 +10,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $task = ['task1', 'task2', 'task3', 'task4', 'task5', 'task6', 'task7', 'task8', 'task9', 'task10'];
         for ($i = 0; $i < 10; $i++) {
             $article = new Article();
-            $article->setTitre('Titre ' . $i);
+            $article->setTitre($task[$i]);
             $article->setDone($i % 2 === 0); // Alternating done status for variety
 
             $manager->persist($article);
