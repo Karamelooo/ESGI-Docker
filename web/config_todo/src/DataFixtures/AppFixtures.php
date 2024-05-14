001a0e006bde634ec43e8b9c7a66b25c8e95d257
Valouche <valentin.arthur1000@gmail.com>
2024-05-14 17:15:12 +0200

    Changing AppFixtures

--- web/config_todo/src/DataFixtures/AppFixtures.php
+++ web/config_todo/src/DataFixtures/AppFixtures.php
@@ -4,24 +4,18 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
 use App\Entity\Article;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker\Factory::create('fr_FR');
-
-        $articles = Array();
-
         for ($i = 0; $i < 10; $i++) {
-            $articles[$i] = new Article();
-            $articles[$i]->setTitre($faker->realText($maxNbChars = 50, $indexSize = 2));
-            $doneBoolean = $faker->boolean;
-            $articles[$i]->setDone($doneBoolean);
+            $article = new Article();
+            $article->setTitre('Titre ' . $i);
+            $article->setDone($i % 2 === 0); // Alternating done status for variety
 
-            $manager->persist($articles[$i]);
+            $manager->persist($article);
         }
 
         $manager->flush();
